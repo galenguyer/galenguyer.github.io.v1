e3ea5a1659f9c1fc04aa43180e9c930016ea7efb
Galen Guyer <git@galenguyer.com>
2020-04-27 15:19:59 -0700

    Add sufjan as a service

---
+++ saas/precache-manifest.c79b3b42641dbb6031007bbe0f900fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a275dc0f31051404a5e478d85f75501",
+    "url": "/saas/index.html"
+  },
+  {
+    "revision": "f0ba81930eff65aeac30",
+    "url": "/saas/static/css/main.d0f745f1.chunk.css"
+  },
+  {
+    "revision": "6dc9853ce12a8512e7d3",
+    "url": "/saas/static/js/2.6dc8f356.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/saas/static/js/2.6dc8f356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ba81930eff65aeac30",
+    "url": "/saas/static/js/main.5766f08a.chunk.js"
+  },
+  {
+    "revision": "279b00a0d1d75bc01e50",
+    "url": "/saas/static/js/runtime-main.28eb0139.js"
+  }
+]);
