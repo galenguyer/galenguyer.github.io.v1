f58b633d1dbe708bdead0dfc19de5f62c7c0fd5e
Galen Guyer <git@galenguyer.com>
2020-04-27 15:29:07 -0700

    Add spotify recent plays page

---
+++ spotify/precache-manifest.6268a92ab9152ac6d85f1cd52719822e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0c68407742b4f9f046eedec3305aab",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "8dd18b18b6a136ca3534",
+    "url": "/spotify/static/css/main.1bd7517b.chunk.css"
+  },
+  {
+    "revision": "e1e2b470bb7d1b880288",
+    "url": "/spotify/static/js/2.305d3a0f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spotify/static/js/2.305d3a0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8dd18b18b6a136ca3534",
+    "url": "/spotify/static/js/main.c16ce96c.chunk.js"
+  },
+  {
+    "revision": "5f791f56b33bdc9010d4",
+    "url": "/spotify/static/js/runtime-main.9da8cb0b.js"
+  }
+]);
